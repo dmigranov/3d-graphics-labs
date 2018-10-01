bd8c0f147dc0d09c1b42748788616122f94c6894
Denis Migranov <d.migranov@g.nsu.ru>
2018-10-01 15:23:47 +0700

    engine added

---
+++ GraphicsEngine/Sources/Tasks/FpsCounter.h
@@ -0,0 +1,33 @@
+﻿#pragma once
+#include "GraphicsEngine/Component.h"
+#include "GraphicsEngine/GUI.h"
+#include "GraphicsEngine/Object.h"
+#include "GraphicsEngine/Transform.h"
+#include "GraphicsEngine/Time.h"
+#include "GraphicsEngine/Vector3.h"
+
+
+class FpsCounter : public Component
+{
+	double	t1;
+	int		f1;
+	double	fps;
+
+public:
+	FpsCounter() 
+	{
+		t1 = Time::GetTime();
+		f1 = Time::GetFrameCount();
+		fps = 0;
+	}
+
+	virtual ~FpsCounter() {}
+
+	virtual void Update() 
+	{
+		double t2	= Time::GetTime();
+		int f2		= Time::GetFrameCount();
+
+		// TODO: Task03
+	}
+};
---
+++ GraphicsEngine/Sources/Tasks/Task03.h
@@ -0,0 +1,56 @@
+﻿#pragma once
+#include "GraphicsEngine/Application.h"
+#include "GraphicsEngine/Camera.h"
+#include "GraphicsEngine/GraphicsEngine.h"
+#include "GraphicsEngine/Materials/MaterialUnlitSimple.h"
+#include "GraphicsEngine/Meshes/MeshCube.h"
+#include "GraphicsEngine/Object.h"
+#include "GraphicsEngine/Scene.h"
+#include "GraphicsEngine/Transform.h"
+
+#include "Tasks/Task.h"
+#include "Tasks/FpsCounter.h"
+#include "Tasks/ObjectRotator.h"
+
+
+/**
+* @brief Shows how to write text to the screen.
+* Key code is situated in the classes FpsCounter, GUI, InternalGUI, DX9GUI, GL20GUI.
+*/
+class Task03: public Task
+{
+public:
+	virtual ~Task03() { }
+
+	virtual void Init()
+	{
+		Scene & scene = Application::Instance().GetScene();
+	
+		// Camera
+		{
+			Object * pCameraObj = new Object();
+			pCameraObj->m_pTransform = new Transform( Vector3(0, 3, -10), Vector3(20.0f, 0.0f, 0.0f));
+			Camera * pCamera = new Camera();
+			pCamera->SetFovY(90);
+			pCamera->SetNearPlane(0.01f);
+			pCamera->SetFarPlane(100);
+			pCameraObj->AddComponent( pCamera );
+
+			scene.SetCamera( pCamera );
+		}		
+		
+		// FPS counter
+		{
+			Object * pObject = new Object();
+			pObject->m_pTransform	= new Transform();
+			pObject->AddComponent( new FpsCounter() );
+
+			scene.AddObject( pObject );
+		}
+	}
+
+	virtual void Update()
+	{
+		
+	}
+};
