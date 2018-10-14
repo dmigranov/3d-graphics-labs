d800a10ad0bf089998fee0adc3d2534f685b2c04
Денис  Мигранов <d.migranov@g.nsu.ru>
2018-10-14 18:02:16 +0700

    smth

--- GraphicsEngine/Sources/main.cpp
+++ GraphicsEngine/Sources/main.cpp
@@ -24,7 +24,7 @@ int main(int argc, char ** argv)
 
     //Task * pTask = new Task01(); //done using plain DirectX 9
     //Task * pTask = new Task02(); //done
-    Task * pTask = new Task03();
+    Task * pTask = new Task03(); //done
     //Task * pTask = new Task04();
     //Task * pTask = new Task05_01();
     //Task * pTask = new Task05_02();
