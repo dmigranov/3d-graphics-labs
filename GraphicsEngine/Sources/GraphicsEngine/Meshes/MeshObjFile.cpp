ad3a53484083baaebc70211d5d69cac6bfb9b872
Денис  Мигранов <d.migranov@g.nsu.ru>
2018-11-17 20:11:55 +0700

    started lab 7: readng object files. now have to parse.

--- GraphicsEngine/Sources/GraphicsEngine/Meshes/MeshObjFile.cpp
+++ GraphicsEngine/Sources/GraphicsEngine/Meshes/MeshObjFile.cpp
@@ -16,6 +16,24 @@ void MeshObjFile::Init()
 	Mesh::Init();
 
 	// TODO: Task07
+	std::vector<Vector3> vertices;
+	std::vector<Vector4> colors; //?
+	std::vector<Vector3> normals;
+	std::vector<Vector3> uv0;
+
+	std::ifstream infile;
+	infile.open(m_filepath);
+
+	if (!infile)
+	{
+		std::cerr << "Unable to open file " << m_filepath << std::endl;
+		exit(1);
+	}
+
+	std::string str;
+	while (std::getline(infile, str))
+		std::cout << str << std::endl;
+
 }
 
 void MeshObjFile::Deinit()
