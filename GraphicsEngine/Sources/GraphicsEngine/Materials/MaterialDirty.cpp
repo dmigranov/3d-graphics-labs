9cafb9b2cde6548f32bdcf54552a2f08d1e6d882
Денис  Мигранов <d.migranov@g.nsu.ru>
2019-01-18 22:37:58 +0700

    bad

--- GraphicsEngine/Sources/GraphicsEngine/Materials/MaterialDirty.cpp
+++ GraphicsEngine/Sources/GraphicsEngine/Materials/MaterialDirty.cpp
@@ -2,6 +2,9 @@
 #include "GraphicsEngine/Transform.h"
 #include "GraphicsEngine/MathUtils.h"
 #include "GraphicsEngine/SceneUtils.h"
+#include "GraphicsEngine/GraphicsApi/DirectX9/DX9GraphicsEngineContext.h"
+
+#include <d3d9.h>
 
 
 MaterialDirty::MaterialDirty(TextureFilterMode filterMode)
@@ -17,8 +20,7 @@ void MaterialDirty::Init()
 {
 	Material::Init();
 
-	m_pTexture1 = new Texture2D("sand.jpg");
-			
+	m_pTexture1 = new Texture2D("dirt.png");
 	m_pTexture1->SetFilterMode(m_filterMode);
 }
 
@@ -37,6 +39,16 @@ void MaterialDirty::SetMaterial(const Object * pObject)
 	const Matrix4x4 & matProj	= SceneUtils::GetMatrixProj();
 	
 	const Matrix4x4 matWorldViewProjT = MathUtils::GetMatrixWorldViewProjT(matWorld, matView, matProj);
+
+
+	
+	//yes, this is bad...
+	const GraphicsEngineContext * pContext = Application::Instance().GetContext();
+	const DX9GraphicsEngineContext * pDX9Context = static_cast<const DX9GraphicsEngineContext *>(pContext);
+	LPDIRECT3DDEVICE9 m_pDevice = pDX9Context->m_pDevice;
+	m_pDevice->SetRenderState(D3DRS_ALPHABLENDENABLE, TRUE);
+	m_pDevice->SetRenderState(D3DRS_SRCBLEND, D3DBLEND_ONE);
+	m_pDevice->SetRenderState(D3DRS_DESTBLEND, D3DBLEND_ONE);
 
 	SetMaterialBegin();
 	{
